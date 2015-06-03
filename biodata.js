c64dbdf396abbfb85da254420bb67f107e39af67
MKS-Student <mks.student@hackreactor.com>
2015-06-03 10:06:31 -0700

    [data] Sample data in JSON format

--- biodata.js
+++ biodata.js
@@ -1,5 +1,6 @@
 //Biomap biographical data in JSON format
 
+// Samples
 // L.marker([34.439, -118.125]).addTo(map)
 // 			.bindPopup("<b>Pasadena, California</b><br /> Caltech BS Geology 2003").openPopup();	
 
@@ -50,4 +51,79 @@
 */
 // Divide DB into Professional and Personal DB
 // 
+
+var bioData = {
+	"1":{
+		bioType: "duration",
+		eventDate: number,
+		startYear: number,
+		endyear: number,
+		latitude: 3.75,
+		longitude: 8.788,
+		bioType: "professional",
+		bioLinks: {
+			"2":{
+			url: null,
+			title: null,
+			urlDescription: null
+			},
+		locationCountryorState: "Equitorial Guinea" ,
+		locationCity: "Malabo",
+		jobDescription: "Wireline Field Engineer"
+		}
+	},
+	"2":{
+		bioType: "duration",
+		eventDate: "12-12-2005",
+		startYear: 2005,
+		endyear: 2008,
+		latitude: 3.75,
+		longitude: 8.788,
+		bioType: "professional",
+		bioLinks: {
+			"2":{
+			url: null,
+			title: null,
+			urlDescription: null
+			},
+		locationCountryorState: "Alaska" ,
+		locationCity: "Prudhoe Bay",
+		jobDescription: "Wireline Field Engineer"
+		}
+	},
+};
+/*
+	L.marker([3.75, 8.788]).addTo(map)
+			.bindPopup("<b>Malabo, Equitorial Guinea</b><br /> Wireline Field Engineer").openPopup();	
+
+		L.marker([70.2, -148.46]).addTo(map)
+			.bindPopup("<b>Prudhoe Bay, Alaska</b><br /> Wireline Field Engineer").openPopup();	
+		
+		L.marker([61.197, -149.838]).addTo(map)
+			.bindPopup("<b>Anchorage, Alaska</b><br /> Wireline Field Engineer").openPopup();	
+		
+		L.marker([29.96, -90.068]).addTo(map)
+			.bindPopup("<b>Houma, Louisiana</b><br /> Wireline Field Engineer").openPopup();	
+		
+		L.marker([48.8, 2.257]).addTo(map)
+			.bindPopup("<b>Paris, France</b><br /> Well Integrity Train-the-Trainer").openPopup();	
+		
+		L.marker([52.457, 4.606]).addTo(map)
+			.bindPopup("<b>Ijmuiden, Netherlands</b><br /> Well Integrity Training").openPopup();	
+
+		L.marker([29.626, -95.610]).addTo(map)
+			.bindPopup("<b>Sugar Land, Texas</b><br /> InTouch Engineer").openPopup();	
+										
+		L.marker([60.398, 5.3097]).addTo(map)
+			.bindPopup("<b>Bergen, Norway</b><br /> Field Quality Champion").openPopup();	
+		
+		L.marker([10.452, -66.859]).addTo(map)
+			.bindPopup("<b>Caracas, Venezuela</b><br /> Teenager").openPopup();	
+		
+		L.marker([34.439, -118.125]).addTo(map)
+			.bindPopup("<b>Pasadena, California</b><br /> Caltech BS Geology 2003").openPopup();	
+		
+		L.marker([37.774, -122.419]).addTo(map)
+			.bindPopup("<b>San Francisco, California</b><br /> Here!").openPopup();
+*/
 
